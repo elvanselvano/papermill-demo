6a1f906f9983ccaffb518be6949b5f555a2a3d41
Elvan <elvanselvano@yahoo.com>
2022-04-04 10:44:23 +0700

    update input output

--- src/run.py
+++ src/run.py
@@ -2,7 +2,7 @@ import papermill as pm
 import os
 import yaml
 
-CONFIG_PATH = './'
+CONFIG_PATH = ''
 
 def load_config(config_name):
      with open(os.path.join(CONFIG_PATH, config_name)) as file:
@@ -13,13 +13,10 @@ def main():
      config = load_config('config.yaml')
      os.system("papermill --help-notebook ../template/paper-input.ipynb")
      
-     result = pm.execute_notebook(config['dataset']['input_path'],
-                                  config['notebook']['output_path'],
-                                  kernel_name='python3',
-                                  parameters=config['parameters'])
+     pm.execute_notebook(config['dataset']['input_path'],
+                         config['notebook']['output_path'],
+                         parameters=config['parameters'])
      
-     print(result)
-
 if __name__ == '__main__':
      main()
 
